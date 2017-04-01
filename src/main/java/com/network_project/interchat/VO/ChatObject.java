2b9b1629008f0094c83b2b66ef40f1fb5837c385
Napping <Napping@119.202.81.211>
2017-04-01 18:02:11 +0900

    Implemented the live time chatting

---
+++ src/main/java/com/network_project/interchat/VO/ChatObject.java
@@ -0,0 +1,22 @@
+package com.network_project.interchat.VO;
+
+public class ChatObject {
+	private String user;
+	private String content;
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
